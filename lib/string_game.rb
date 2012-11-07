9695ba6ad294a3104d2cd62f2c2c2c3edf7c71d2
victor <vvazquez@prodimar.com>
2012-11-07 11:13:44 -0800

    antes de merge

--- lib/string_game.rb
+++ lib/string_game.rb
@@ -1,2 +1,28 @@
 class StringGame
+	def initialize(s)
+		@s = s
+	end
+
+	def palindromo?
+		(@s == @s.reverse)? true : false
+	end
+
+	def numero_de_letras
+		@s.length
+	end
+
+	def numero_de_palabras
+		@s.scan(/(\w+)/).length
+	end
+
+	def pon_nombre(nombre)
+		@s.sub("nombre", nombre)
+	end
+
+	def self.saluda(nombres)
+		return "Hola #{nombres}!!!" if nombres.class != Array
+		str = "Hola #{nombres.shift}"
+		nombres.each { |n| str += ", hola #{n}" }
+		str
+	end
 end
