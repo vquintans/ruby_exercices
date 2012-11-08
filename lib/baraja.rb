ae7382134d3a7cccd28c8dc78f5f0549281c6039
saasbook <saasbook@saasbook.(none)>
2012-11-08 07:21:15 -0800

    commit 8 nov prueba

--- lib/baraja.rb
+++ lib/baraja.rb
@@ -1,3 +1,40 @@
 class Baraja
 
+	attr_accessor :cartas
+
+	def initialize(*cartas)
+		@cartas = cartas
+	end
+
+	def numero_de_cartas
+		@cartas.length
+	end
+
+	def contiene_carta?(carta)
+		@cartas.include?(carta)
+	end
+
+	def add_carta(carta)
+		@cartas.push(carta)
+	end
+
+	def self.completa
+		b = Baraja.new()
+
+		Carta.palos.each do |palo| 
+			for i in 1..12 b.add_carta(Carta.new(palo, i))
+		end
+
+		#for i in 1..12		
+		#	b.add_carta(Carta.new(:oros, i))
+		#	b.add_carta(Carta.new(:copas, i))
+		#	b.add_carta(Carta.new(:bastos, i))
+		#	b.add_carta(Carta.new(:espadas, i))
+		#end
+
+		b
+	end
+
+	def ordenada
+	end 
 end
--- lib/string_game.rb
+++ lib/string_game.rb
@@ -1,2 +1,39 @@
 class StringGame
+	
+	def initialize(cadena)
+		@str = String.new(cadena)
+	end
+
+	def palindromo?
+		@str == @str.reverse
+	end
+
+	def numero_de_letras
+		@str.length
+	end
+
+	def numero_de_palabras
+		@str.split.length
+	end
+
+	def pon_nombre(nombre)
+		@str.gsub("nombre", nombre)
+	end
+
+	def self.saluda(nombre)
+		if tmp = Array.try_convert(nombre)
+  			# the argument is an array
+  			result = ""
+  			
+  			nombre.each {|n| result += ", hola " + n } 
+  			result = result[2..-1]
+  			result[0] = 'H'
+  			result
+  			
+		elsif tmp = String.try_convert(nombre)
+  			# the argument is a string
+  			"Hola " + nombre + "!!!"
+		end		
+	end
+
 end
